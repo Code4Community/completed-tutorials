50c5e2d0d07caf3d14b731a940b4e6dabf69cfc6
Alex <alex.wimmel@gmail.com>
2020-08-31 08:57:02 -0400

    adding node npm tutorial finished docs

---
+++ node-npm-tutorial/tutorial.js
@@ -0,0 +1,18 @@
+
+var c4c = [];
+c4c.push("Community service with computer science");
+c4c.push("Mindup");
+c4c.push("School Outreach");
+var numbers = [];
+numbers.push(1);
+numbers.push(2);
+numbers.push(3);
+numbers.push(5);
+numbers.push(8);
+var combinedObject = {
+   codeInfo: c4c,
+   fibonacci: numbers
+};
+console.log(c4c);
+console.log(numbers);
+console.log(combinedObject);
